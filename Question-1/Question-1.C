1d0d03bbd1a51ee4d2e1b4bdae28a97ed168106b
Himanshu Chandna <himanshuchandna0007@gmail.com>
2023-10-14 00:21:35 +0530

    Question-1

---
+++ Question-1/Question-1.C
@@ -0,0 +1,26 @@
+// Write a program to study variables and constants of int and float data-types
+
+#include<stdio.h>
+
+int main(){
+
+    int a, b = 100;
+    float c, d = 198.234;
+
+    a = 50;
+    c = 234.567;
+
+    printf("Name-Himanshu Chandna, Class-1B\n\n");
+
+    printf("Write a program to study variables and constants of int and float data-types\n\n");
+
+    printf("Variable Integer Number: %d\n",a);
+    printf("Constant Integer Number: %d\n",b);
+    printf("Variable Floating Number: %f\n",c);
+    printf("Constant Floating Number: %f\n\n",d);
+
+    printf("<--- End of Code --->");
+
+    return 0;
+
+}
